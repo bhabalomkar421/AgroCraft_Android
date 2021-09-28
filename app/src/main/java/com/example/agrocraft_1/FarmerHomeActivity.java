80af5b2383962d1670039d3164846737ce24f483
bhabalomkar421 <bhabalomkar421@gmail.com>
2021-09-28 23:35:00 +0530

    Farmer Registration Completed

--- app/src/main/java/com/example/agrocraft_1/FarmerHomeActivity.java
+++ app/src/main/java/com/example/agrocraft_1/FarmerHomeActivity.java
@@ -3,13 +3,12 @@ package com.example.agrocraft_1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.support.v4.app.*;
 
-public class HomeActivity extends AppCompatActivity {
+public class FarmerHomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_farmer_home);
     }
 }
